1d1a3cbb69bb32a09dc0149358d12f1651936842
Renan Ribeiro <rphiliper@gmail.com>
2025-10-04 01:28:22 -0300

    Add navbar script

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,34 @@
+// Navbar
 document.addEventListener("DOMContentLoaded", () => {
-  const alvo = document.querySelector("#nome");
-  if (alvo) {
-    alvo.scrollIntoView({ behavior: "smooth", block: "start" });
-  }
+
+    const toggle = document.getElementById("menu-toggle");
+    const menu = document.getElementById("menu");
+
+    // Toggle abr/fecha (evita propagação)
+    toggle.addEventListener("click", (e) => {
+        menu.classList.toggle("show");
+        e.stopPropagation();
+    });
+
+    // Fecha ao clicar fora
+    document.addEventListener("click", (e) => {
+        if (menu.classList.contains("show")) {
+            if (!menu.contains(e.target) && !toggle.contains(e.target)) {
+                menu.classList.remove("show");
+            }
+        }
+    });
+
+    // Fecha com ESC
+    document.addEventListener("keydown", (e) => {
+        if (e.key === "Escape") menu.classList.remove("show");
+    });
+});
+
+// Name Smooth
+document.addEventListener("DOMContentLoaded", () => {
+    const alvo = document.querySelector("#nome");
+    if (alvo) {
+        alvo.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
 });
